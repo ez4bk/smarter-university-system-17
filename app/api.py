18bc93dbcffb124035efab7fc9df7e08f27018f2
ez4bk <qq1034351717@gmail.com>
2023-10-11 23:28:33 -0400

    Add react frontend

--- app/api.py
+++ app/api.py
@@ -3,24 +3,24 @@ import json
 
 from app.controllers.activities_controller import ActivitiesController
 
-activities = ActivitiesController()
+
 from app.controllers.discussions_controller import DiscussionsController
 
+activities = ActivitiesController()
 discussions = DiscussionsController()
+controller = Blueprint("config", __name__)
 
-controller = Blueprint('config', __name__)
 
-
-@controller.route('/discussions', methods=['GET'])
+@controller.route("/discussions", methods=["GET"])
 def get_discussions():
     return discussions.get_discussions()
 
 
-@controller.route('/discussion', methods=['POST'])
+@controller.route("/discussion", methods=["POST"])
 def add_discussion():
     return discussions.add_discussion_from_payload(request.data.decode(), request.args)
 
 
-@controller.route('/activity', methods=['POST'])
+@controller.route("/activity", methods=["POST"])
 def add_activity():
     return activities.add_activity_from_payload(request.data.decode(), request.args)
--- app/server.py
+++ app/server.py
@@ -1,16 +1,18 @@
-
-
 from flask import Flask
 from api import controller
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static'
+app.config["UPLOAD_FOLDER"] = "static"
 app.register_blueprint(controller)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9083)
+# @app.route('/')
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9083)
